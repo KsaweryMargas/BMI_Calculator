d2c2069e5f53f35c4b8b39185901877d22135cbf
Ksawery Margas <ksawerymargas@gmail.com>
2025-12-18 21:14:51 +0100

    Logika liczenia i interfejs

--- BMI Calculator/Form1.Designer.cs
+++ BMI Calculator/Form1.Designer.cs
@@ -1,4 +1,8 @@
-﻿namespace BMI_Calculator
+﻿using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BMI_Calculator
 {
     partial class Form1
     {
@@ -28,12 +32,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            oblicz = new Button();
+            w = new Label();
+            label2 = new Label();
+            wynikBMI = new Label();
+            wynikKategoria = new Label();
+            wpiszweight = new TextBox();
+            wpiszheight = new TextBox();
+            height = new Label();
+            weight = new Label();
+            age = new Label();
+            wpiszwiek = new TextBox();
+            SuspendLayout();
+            // 
+            // oblicz
+            // 
+            oblicz.BackColor = Color.SeaGreen;
+            oblicz.ForeColor = SystemColors.ButtonHighlight;
+            oblicz.Location = new Point(22, 213);
+            oblicz.Name = "oblicz";
+            oblicz.Size = new Size(83, 40);
+            oblicz.TabIndex = 2;
+            oblicz.Text = "Submit";
+            oblicz.UseVisualStyleBackColor = false;
+            oblicz.Click += oblicz_Click;
+            // 
+            // w
+            // 
+            w.AutoSize = true;
+            w.Location = new Point(27, 281);
+            w.Name = "w";
+            w.Size = new Size(31, 15);
+            w.TabIndex = 3;
+            w.Text = "BMI:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(26, 329);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Kategoria:";
+            // 
+            // wynikBMI
+            // 
+            wynikBMI.AutoSize = true;
+            wynikBMI.Location = new Point(139, 281);
+            wynikBMI.Name = "wynikBMI";
+            wynikBMI.Size = new Size(22, 15);
+            wynikBMI.TabIndex = 5;
+            wynikBMI.Text = "---";
+            // 
+            // wynikKategoria
+            // 
+            wynikKategoria.AutoSize = true;
+            wynikKategoria.Location = new Point(138, 329);
+            wynikKategoria.Name = "wynikKategoria";
+            wynikKategoria.Size = new Size(22, 15);
+            wynikKategoria.TabIndex = 6;
+            wynikKategoria.Text = "---";
+            // 
+            // wpiszweight
+            // 
+            wpiszweight.BackColor = SystemColors.ControlLightLight;
+            wpiszweight.Location = new Point(27, 150);
+            wpiszweight.Name = "wpiszweight";
+            wpiszweight.Size = new Size(135, 23);
+            wpiszweight.TabIndex = 7;
+            wpiszweight.Text = "kg";
+            // 
+            // wpiszheight
+            // 
+            wpiszheight.BackColor = SystemColors.ControlLightLight;
+            wpiszheight.Location = new Point(27, 95);
+            wpiszheight.Name = "wpiszheight";
+            wpiszheight.Size = new Size(135, 23);
+            wpiszheight.TabIndex = 8;
+            wpiszheight.Text = "cm";
+            // 
+            // height
+            // 
+            height.AutoSize = true;
+            height.Location = new Point(27, 77);
+            height.Name = "height";
+            height.Size = new Size(78, 15);
+            height.TabIndex = 9;
+            height.Text = "Wzrost w cm:";
+            // 
+            // weight
+            // 
+            weight.AutoSize = true;
+            weight.Location = new Point(27, 132);
+            weight.Name = "weight";
+            weight.Size = new Size(68, 15);
+            weight.TabIndex = 10;
+            weight.Text = "Waga w kg:";
+            // 
+            // age
+            // 
+            age.AutoSize = true;
+            age.Location = new Point(27, 19);
+            age.Name = "age";
+            age.Size = new Size(36, 15);
+            age.TabIndex = 14;
+            age.Text = "Wiek:";
+            // 
+            // wpiszwiek
+            // 
+            wpiszwiek.Location = new Point(27, 37);
+            wpiszwiek.Name = "wpiszwiek";
+            wpiszwiek.Size = new Size(135, 23);
+            wpiszwiek.TabIndex = 15;
+            wpiszwiek.Text = "wiek";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(510, 453);
+            Controls.Add(wpiszwiek);
+            Controls.Add(age);
+            Controls.Add(weight);
+            Controls.Add(height);
+            Controls.Add(wpiszheight);
+            Controls.Add(wpiszweight);
+            Controls.Add(wynikKategoria);
+            Controls.Add(wynikBMI);
+            Controls.Add(label2);
+            Controls.Add(w);
+            Controls.Add(oblicz);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button oblicz;
+        private Label w;
+        private Label label2;
+        private Label wynikBMI;
+        private Label wynikKategoria;
+        private TextBox wpiszweight;
+        private TextBox wpiszheight;
+        private Label height;
+        private Label weight;
+        private Label age;
+        private TextBox wpiszwiek;
     }
 }
--- BMI Calculator/Form1.cs
+++ BMI Calculator/Form1.cs
@@ -1,10 +1,67 @@
-﻿namespace BMI_Calculator
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace BMI_Calculator
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void oblicz_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(wpiszheight.Text, out double height) ||
+                !double.TryParse(wpiszweight.Text, out double weight) ||
+                !double.TryParse(wpiszwiek.Text, out double wiek))
+            {
+                MessageBox.Show("Wpisz poprawne liczby!");
+                return;
+            }
+
+            double bmi = weight / Math.Pow(height / 100.0, 2);
+            bmi = Math.Round(bmi, 2);
+            wynikBMI.Text = bmi.ToString();
+
+            string kategoria = "";
+
+            if (bmi < 14)
+            {
+                kategoria = "Szczurek";
+            }
+            else if (bmi < 17)
+            {
+                kategoria = "Wychudzenie";
+            }
+            else if (bmi < 20)
+            {
+                kategoria = "Niedowaga";
+            }
+            else if (bmi < 25)
+            {
+                kategoria = "Pożądana masa ciała";
+            }
+            else if (bmi < 30)
+            {
+                kategoria = "Nadwaga";
+            }
+            else if (bmi < 35)
+            {
+                kategoria = "Otyłość I stopnia";
+            }
+            else if (bmi < 40)
+            {
+                kategoria = "Otyłość II stopnia";
+            }
+            else
+            {
+                kategoria = "Michal Jaśkiewicz";
+            }
+
+            wynikKategoria.Text = kategoria;
         }
     }
 }
